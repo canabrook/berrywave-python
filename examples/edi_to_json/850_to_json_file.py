54f3bdf46fb1347d5221663cdb629f4965108895
canabrook <p}#8JW[a'N_k'QV,XH>
2026-08-08 16:05:40 -0500

    Examples are self-contained, not relying on specific files to be available

--- examples/edi_to_json/850_to_json_file.py
+++ examples/edi_to_json/850_to_json_file.py
@@ -2,38 +2,49 @@
 Convert an X12 850 Purchase Order to JSON.
 
 This example demonstrates converting EDI to JSON where the input and output
-are in files instead of Python strings.
+are files instead of Python strings.
 """
+
 from pathlib import Path
+from tempfile import TemporaryDirectory
+
 from berrywave import EdiService
 
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+EDI_DOCUMENT = """
+ISA^00^          ^00^          ^ZZ^1556150        ^12^5088942073     ^100903^0143^U^00401^500009740^0^P^|~
+GS^PO^007941230^8145^20100903^0143^653^X^004010~
+ST^850^8620~
+BEG^00^SA^16683^^20100902~
+N1^ST^OWENS & MINOR - HOUSTON^92^056551~
+PO1^001^2^CA^74.8692^UM^VC^130^IN^0565000130~
+PO1^002^3^CA^95.5^UM^VC^0565TRN1184^IN^0565TRN1184~
+CTT^Q2~
+SE^7^8620~
+GE^1^653~
+IEA^1^500009740~
+"""
 
 
 def main():
-    input_file = (
-            PROJECT_ROOT
-            / "sample_data"
-            / "purchase_order_850.edi"
-    )
-
-    output_file = (
-            PROJECT_ROOT
-            / "output"
-            / "850.json"
-    )
-
-    if not input_file.exists():
-        raise FileNotFoundError(
-            f"Input file not found: {input_file}"
-        )
-
     service = EdiService()
 
-    service.edi_to_json_file(
-        input_file,
-        output_file,
-    )
+    with TemporaryDirectory() as temp_dir:
+        temp_path = Path(temp_dir)
+
+        input_file = temp_path / "850.edi"
+        output_file = temp_path / "850.json"
+
+        input_file.write_text(EDI_DOCUMENT.strip())
+
+        service.edi_to_json_file(
+            input_file,
+            output_file,
+        )
+
+        json_document = output_file.read_text()
+
+        print(json_document)
 
 
 if __name__ == "__main__":
